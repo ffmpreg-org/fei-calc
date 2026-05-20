f439d7b4b3ba41d431dfd703535bae5df50c5ab9
ffmpreg-org <289032686+ffmpreg-org@users.noreply.github.com>
2026-05-20 19:22:05 +0200

    Content: Update favicon, scaffold sidebar

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,33 @@
+<script setup>
+import { useLocalStorage } from "@vueuse/core"
+
+const isSidebarOpen = useLocalStorage("isSidebarOpen", true)
+</script>
+
 <template>
 	<UApp>
 		<UMain>
-			<NuxtPage />
+			<div class="flex flex-1">
+				<USidebar
+					:open="isSidebarOpen"
+					collapsible="icon"
+				>
+					<template #footer>
+						<UButton
+							:icon="
+								isSidebarOpen
+									? 'i-lucide-panel-left-close'
+									: 'i-lucide-panel-left-open'
+							"
+							class="ml-auto"
+							variant="ghost"
+							color="neutral"
+							@click="isSidebarOpen = !isSidebarOpen"
+						/>
+					</template>
+				</USidebar>
+				<NuxtPage />
+			</div>
 		</UMain>
 	</UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,12 @@ export default defineNuxtConfig({
 		enabled: true
 	},
 
+	app: {
+		head: {
+			link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }]
+		}
+	},
+
 	css: ["~/assets/css/main.css"],
 
 	routeRules: {
@@ -21,5 +27,5 @@ export default defineNuxtConfig({
 				braceStyle: "1tbs"
 			}
 		}
-	}
+	},
 })
