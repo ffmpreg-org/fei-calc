940f3b7e55043abb85f0d7e1cb4d1d76e659e867
ffmpreg-org <289032686+ffmpreg-org@users.noreply.github.com>
2026-05-20 20:23:32 +0200

    Content: More sidebar scaffold

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,33 @@
 import { useLocalStorage } from "@vueuse/core"
 
 const isSidebarOpen = useLocalStorage("isSidebarOpen", true)
+
+const navMenuItems = [
+	{
+		label: "Home",
+		icon: "i-lucide-home",
+		href: "/"
+	},
+	{
+		label: "TO",
+		icon: "i-lucide-book-text",
+		children: [
+			{
+				label: "TO I",
+				href: "/to-1"
+			},
+			{
+				label: "TO II",
+				href: "/to-2"
+			}
+		]
+	},
+	{
+		label: "General",
+		icon: "i-lucide-calculator",
+		href: "/general"
+	}
+]
 </script>
 
 <template>
@@ -9,22 +36,42 @@ const isSidebarOpen = useLocalStorage("isSidebarOpen", true)
 		<UMain>
 			<div class="flex flex-1">
 				<USidebar
-					:open="isSidebarOpen"
+					v-model:open="isSidebarOpen"
 					collapsible="icon"
+					:style="{
+						'--sidebar-width-icon': '5.5rem'
+					}"
+					:rail="true"
+					:ui="{}"
 				>
-					<template #footer>
+					<template #default>
+						<UNavigationMenu
+							:items="navMenuItems"
+							orientation="vertical"
+							size="lg"
+							:ui="{
+								link: 'data-active:before:bg-primary-500/20 px-4 py-3 gap-2 min-w-14 overflow-hidden',
+								linkLeadingIcon: 'size-6',
+								linkLabel: 'text-base font-medium'
+							}"
+							:popover="true"
+							:arrow="true"
+							:collapsed="!isSidebarOpen"
+						/>
+					</template>
+					<!-- <template #footer>
 						<UButton
 							:icon="
 								isSidebarOpen
 									? 'i-lucide-panel-left-close'
 									: 'i-lucide-panel-left-open'
 							"
-							class="ml-auto"
+							size="lg"
 							variant="ghost"
 							color="neutral"
 							@click="isSidebarOpen = !isSidebarOpen"
 						/>
-					</template>
+					</template> -->
 				</USidebar>
 				<NuxtPage />
 			</div>
