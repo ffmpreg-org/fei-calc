66e3e48fcb8ecef081ea79ddb9c89241e941d56a
ffmpreg-org <289032686+ffmpreg-org@users.noreply.github.com>
2026-05-20 16:48:19 +0200

    Style: Colors, fonts, test template

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="flex flex-col items-center justify-center h-screen">
+		<div class="max-w-xl">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi eos illo aspernatur earum suscipit labore
+			deleniti
+			dolores minima? In aut corrupti quos voluptas exercitationem eos non repudiandae blanditiis accusamus ullam.
+		</div>
+	</div>
+</template>
